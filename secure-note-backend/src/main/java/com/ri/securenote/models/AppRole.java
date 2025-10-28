90ee381cfd3ae3b92d81710ef44e249bd3e4775a
AsadurRahmanRishan59 <asadur.rahman.rishan@g.bracu.ac.bd>
2025-10-28 17:40:50 +0600

    working with JdbcUserDetailsManager

---
+++ secure-note-backend/src/main/java/com/ri/securenote/models/AppRole.java
@@ -0,0 +1,6 @@
+package com.ri.securenote.models;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_ADMIN,
+}
---
+++ secure-note-backend/src/main/java/com/ri/securenote/models/Role.java
@@ -0,0 +1,17 @@
+package com.ri.securenote.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "roles")
+public class Role {
+    
+    private
+}
