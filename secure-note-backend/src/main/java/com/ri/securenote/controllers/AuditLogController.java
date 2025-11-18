36f22eb4503149a629d12b861af88052e524295a
AsadurRahmanRishan59 <asadur.rahman.rishan@g.bracu.ac.bd>
2025-11-18 17:36:35 +0600

    audit log api is working

--- secure-note-backend/src/main/java/com/ri/securenote/controllers/AuditLogController.java
+++ secure-note-backend/src/main/java/com/ri/securenote/controllers/AuditLogController.java
@@ -3,6 +3,7 @@ package com.ri.securenote.controllers;
 import com.ri.securenote.models.AuditLog;
 import com.ri.securenote.services.AuditLogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,14 @@ public class AuditLogController {
     private AuditLogService auditLogService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public List<AuditLog> getAuditLog() {
         return auditLogService.getAllAuditLogs();
     }
 
+
     @GetMapping("/note/{noteId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<AuditLog> getAuditLogByNoteId(@PathVariable Long noteId) {
         return auditLogService.getAuditLogsByNoteId(noteId);
     }
